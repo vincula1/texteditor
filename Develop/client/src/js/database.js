f736edf7c8c63c0fbbb05694d30adc87254007a1
vincula1 <141301612+vincula1@users.noreply.github.com>
2023-12-02 07:45:56 -0600

    Did to do for database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,47 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  export const putDb = async (content) => {
+    console.log('PUT to the database');
+  
+    // Open a connection to the database
+    const jateDb = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges
+    const tx = jateDb.transaction('jate', 'readwrite');
+  
+    // Open up the desired object store
+    const store = tx.objectStore('jate');
+  
+    // Use put to store value
+    const request = store.put({ id: 1, value: content });
+  
+    // Get confirmation of the request
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
+  
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  export const getDb = async () => {
+    console.log('GET from the database');
+  
+    // Open a connection to the database
+    const jateDb = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges
+    const tx = jateDb.transaction('jate', 'readonly');
+  
+    // Open up the desired object store
+    const store = tx.objectStore('jate');
+  
+    // Get info with id I
+    const request = store.get(1);
+  
+    // Get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  };
+  
 
 initdb();
